d9095c95f5efc8b1940fafb2555e6982251550b8
Niila Viinamäki <niila.viinamaki@proton.me>
2025-08-19 22:21:37 +0300

    gitsigns colors change

--- init.lua
+++ init.lua
@@ -3,6 +3,20 @@ require('config.lazy')
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
 
+require('gitsigns').setup {
+  signs = {
+    add          = { text = '▎' },
+    change       = { text = '▕' },
+    delete       = { text = '▁' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '≃' },
+  },
+}
+vim.api.nvim_set_hl(0, 'GitSignsAdd',    { fg = '#00ff00'})
+vim.api.nvim_set_hl(0, 'GitSignsChange', { fg = '#00ffff'})
+vim.api.nvim_set_hl(0, 'GitSignsDelete', { fg = '#ff0000'})
+
+
 vim.opt.number = true
 vim.opt.cursorline = true
 
